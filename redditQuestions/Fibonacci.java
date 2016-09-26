f75822f1aea3aafe23350fe7da82cfcb81f4b75b
Shreyas MN <shreyaschinni@gmail.com>
2016-09-26 17:19:05 -0400

    Create Fibonacci.java

---
+++ redditQuestions/Fibonacci.java
@@ -0,0 +1,23 @@
+package redditQuestions;
+
+/**
+ * @author ${Shreyas Mahanthappa Nagaraj}
+ * 
+ */
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		Fibonacci fb = new Fibonacci();
+		fb.printFibonacci(14);
+	}
+
+	/**
+	 * 
+	 */
+	private void printFibonacci(int n) {
+		if(n <=1)
+		System.out.print(""+0 + " 1");
+		
+		long[] fob = new long[n];
+	}
+}
