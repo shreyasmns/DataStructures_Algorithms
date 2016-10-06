3890c924fe41089bf6db3822d6d30c6ef7928117
Shreyas MN <shreyaschinni@gmail.com>
2016-10-06 13:28:20 -0400

    Add files via upload

---
+++ google/NextSamllestInt.java
@@ -0,0 +1,20 @@
+package google;
+
+/**
+ * @author ${Shreyas Mahanthappa Nagaraj}
+ * 
+ */
+public class NextSamllestInt {
+
+	public static void main(String[] args) {
+		Node root1 = new Node(15);
+		root1.left = new Node(10);
+		root1.right = new Node(20);
+		root1.left.left = new Node(5);
+		root1.left.right = new Node(12);
+		root1.right.left = new Node(18);
+		root1.right.right = new Node(26);
+	
+	}
+
+}
